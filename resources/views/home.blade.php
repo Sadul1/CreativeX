ac84e620077f9439a98f50a8b779589db9f562c9
sadul1 <rasasara5@gmail.com>
2021-06-07 22:55:41 +0530

    07-06-2021_update2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Creative X | Creative Digital Agency</title>
-<!-- <meta name="author" content="Themezinho"> -->
-<meta name="Creative X" content="Creative Digital Agency">
-<meta property="og:description" content="Creative X | Creative Digital Agency">
+<meta name="description" content="">
+
 <!-- <meta name="keywords" content="wave, creatve, portfolio, html, template, carousel, theme, agency, html5"> -->
 
 <!-- SOCIAL MEDIA META -->
